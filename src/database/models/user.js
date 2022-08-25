e561d8e6688d4c7f0ec06ff1b999038d1f506ca1
Joao <me.contrate.aero@gmail.com>
2022-08-25 18:35:47 -0300

    req02 criado model de user

---
+++ src/database/models/user.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    displayName: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+  });
+  User.associate = (models) => {
+    User.hasOne(models.BlogPosts, {
+      foreignKey: 'userId',
+      as: 'blogPosts',
+    });
+  }
+  return User;
+};
