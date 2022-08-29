fd3f078fa72cb67274368748852ebd4a6a476fdd
Joao <me.contrate.aero@gmail.com>
2022-08-29 20:00:27 -0300

    requisito 4 testado

--- src/database/middlewares/Validators.js
+++ src/database/middlewares/Validators.js
@@ -1,34 +1,31 @@
-const incorrectDisplayName = '"displayName" length must be at least 8 characters long';
-const incorrectEmail = '"email" must be a valid email';
-const incorrectPassword = '"password" length must be at least 6 characters long';
+const inDisNam = '"displayName" length must be at least 8 characters long';
+const inEmail = '"email" must be a valid email';
+const inPass = '"password" length must be at least 6 characters long';
+const emailchecker = /\S+@\S+\.\S+/;
 
-const validateDisplayName = (displayName, res) => {
-    if (!displayName || displayName.length < 8) {
-        return res.status(400)
-        .json({ message: incorrectDisplayName });
-    }
-};
+// const validateDisplayName = (displayName, res) => {
+//     if (!displayName || displayName.length < 8) return res.status(400).json({ message: incorrectDisplayName });
+   
+// };
 
-const validateEmail = (email, res) => {
-    if (!email || !email.includes('@')) {
-        return res.status(400)
-        .json({ message: incorrectEmail });
-    }
-};
+// const validateEmail = (email, res) => {
+//     if (!email || !email.includes(emailchecker)) return res.status(400).json({ message: incorrectEmail });
+//     }
+// };
 
-const validatePassword = (password, res) => {
-    if (!password || password.length < 6) {
-        return res.status(400)
-        .json({ message: incorrectPassword });
-    }
-};
+// const validatePassword = (password, res) => {
+//     if (!password || password.length < 6) return res.status(400).json({ message: incorrectPassword });
+//     }
+// };
 
 function validate(req, res, next) {    
     const { displayName, email, password } = req.body;
-    validateDisplayName(displayName, res);
-    validateEmail(email, res);
-    validatePassword(password, res);
-next();
+    if (displayName.length < 8) return res.status(400).json({ message: inDisNam });
+   
+    if (!emailchecker.test(email)) return res.status(400).json({ message: inEmail });
+    
+    if (password.length < 6) return res.status(400).json({ message: inPass });
+    next();
 }
 
 module.exports = { validate };
